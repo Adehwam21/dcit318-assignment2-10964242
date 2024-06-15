d7fd30b7bf030f4811b7c8d2a3b456db1d8e95ec
Adehwam21 <drewaaron96@gmail.com>
2024-06-15 14:31:27 +0000

    Add Inheritance and Method Overriding solution

---
+++ InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,37 @@
+﻿
+public class Program
+{
+    // Base class
+    public class Animal 
+    {
+        public virtual string MakeSound()
+        {
+            return "Some generic sound";
+        }
+    }
+
+    public class Dog : Animal   // Inheritance
+    {
+        public override string MakeSound()  //  Overrides MakeSound property of the base class
+        {
+            return "Bark";
+        }
+    }
+
+    public class Cat : Animal   // Inheritance
+    {
+        public override string MakeSound()  //Overrides MakeSound property of the base class
+        {
+            return "Meow";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        // Instances of the Dog and Cat class
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        Console.WriteLine($"Dogs {dog.MakeSound()} while Cats {cat.MakeSound()}!");
+    }
+}
