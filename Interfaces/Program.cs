f0983a9c87f79669d08f7608e85f80f49f4433b9
Adehwam21 <drewaaron96@gmail.com>
2024-06-16 00:28:24 +0000

    Add Interfaces solution

---
+++ Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿
+class Program
+{
+    interface IMovable
+    {
+        void Move();
+
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving.");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bike = new Bicycle();
+
+        car.Move();
+        bike.Move();
+    }
+}
